f0304d80da0433273aac17b8a1a64822974a29a8
isidorabecker <isidora.becker@uc.cl>
2025-05-31 09:04:26 -0400

    feature(src/routes/router.tsx): ADD QueryClientProvider component

--- pokeProject/src/routes/router.tsx
+++ pokeProject/src/routes/router.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import HomePage from './HomePage'
+import { HomePage } from './HomePage'
+import { PokemonsList } from './PokemonList'
+import { QueryClientProvider } from './QueryClient'
 
 export const ApplicationRouter: any = () => {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <HomePage />
+      element: <QueryClientProvider />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />
+        },
+        {
+          path: '/pokemons-list',
+          element: <PokemonsList />
+        }
+      ]
     }
   ])
 
