4c6a0810c6e9ce8416b0e64ceb0b881199e46de8
isidorabecker <isidora.becker@uc.cl>
2025-05-31 14:42:14 -0400

    feature(src/routes/PokemonList.tsx): UPDATE style in component

--- pokeProject/src/routes/PokemonList.tsx
+++ pokeProject/src/routes/PokemonList.tsx
@@ -37,10 +37,12 @@ export const PokemonsList = () => {
   )
 
   return (
-    <section className="h-[500px] flex flex-col justify-start px-10 overflow-y-auto">
-      <div className="flex justify-between items-center mb-4">
-        <h1>Pokemon List</h1>
-        <div className="flex gap-4">
+    <section className="h-full flex flex-col justify-start px-10 overflow-y-auto">
+      <div>
+        <div className="mt-3 flex justify-center font-semibold text-4xl mb-4">
+          <span className="mb-8 text-center text-4xl font-extrabold text-red-600">Pokemon List</span>
+        </div>
+        <div className="flex justify-between mb-3">
           <input
             type="text"
             placeholder="Search Pokémon..."
@@ -48,35 +50,35 @@ export const PokemonsList = () => {
             onChange={handleSearch}
             className="px-4 py-2 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
           />
-          <button onClick={() => handleShowFavorites()}>
+          <button className="bg-red-600 border-radious items-center rounded-lg w-96 h-10 text-white font-semibold" onClick={() => handleShowFavorites()}>
             {buttonLabel}
           </button>
         </div>
+        <article className="mb-4 grid grid-cols-3 gap-4">
+          {displayedPokemons.map((pokemon, index) => (
+            <button onClick={() => console.log(pokemon.name)} key={index}>
+              <div
+                className="flex flex-col items-center p-4 border-2 border-black rounded-lg shadow-md bg-white hover:shadow-lg transition-shadow"
+              >
+                <img
+                  src={pokemon.imageUrl}
+                  alt={pokemon.name}
+                  className="w-24 h-24 object-contain"
+                />
+                <h3 className="mt-2 text-lg font-semibold capitalize">
+                  {pokemon.name}
+                </h3>
+                <button
+                  onClick={() => handleFavorite(pokemon)}
+                  className="flex mt-2 px-4 py-2 bg-red-600 text-white font-semibold w-full rounded-lg hover:bg-red-600"
+                  >
+                  {favorites.includes(pokemon) ? 'Remover de Favoritos' : 'Agregar a Favoritos'}
+                </button>
+              </div>
+            </button>
+          ))}
+        </article>
       </div>
-      <article className="mx-4 mb-4 grid grid-cols-3 gap-4">
-        {displayedPokemons.map((pokemon, index) => (
-          <button onClick={() => console.log(pokemon.name)} key={index}>
-            <div
-              className="flex flex-col items-center p-4 border-2 border-black rounded-lg shadow-md bg-white hover:shadow-lg transition-shadow"
-            >
-              <img
-                src={pokemon.imageUrl}
-                alt={pokemon.name}
-                className="w-24 h-24 object-contain"
-              />
-              <h3 className="mt-2 text-lg font-semibold capitalize">
-                {pokemon.name}
-              </h3>
-              <button
-                onClick={() => handleFavorite(pokemon)}
-                className="mt-2 px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600"
-                >
-                {favorites.includes(pokemon) ? 'Remover de Favoritos' : 'Agregar a Favoritos'}
-              </button>
-            </div>
-          </button>
-        ))}
-      </article>
     </section>
   )
 }
