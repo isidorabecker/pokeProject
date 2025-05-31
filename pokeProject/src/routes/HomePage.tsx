59c52ca13b83ed734945899f10794dc6150ea6f4
isidorabecker <isidora.becker@uc.cl>
2025-05-31 09:05:41 -0400

    feature(src/routes/HomePage.tsx): HANDLE to pokemon list component

--- pokeProject/src/routes/HomePage.tsx
+++ pokeProject/src/routes/HomePage.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const HomePage: React.FC = () => {
+export const HomePage: React.FC = () => {
+  const navigate = useNavigate()
+  const handleNextPage = () => {
+    navigate('/pokemons-list')
+  }
+
   return (
     <section className="h-[500px] flex justify-center px-10">
       <article className="mx-4 mb-4 flex flex-row justify-center">
         <div className="justify-center items-center flex-col h-full w-full">
           <h1> PokeWeb </h1>
-          <button onClick={() => console.log('CLICKED')}> START</button>
+          <button onClick={ () => handleNextPage() }>START</button>
         </div>
       </article>
     </section>
   )
 }
-
-export default HomePage
