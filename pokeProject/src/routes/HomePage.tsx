e2b26b92573fcbb338a1b636fe7d5d61e3dc08b5
isidorabecker <isidora.becker@uc.cl>
2025-05-31 00:20:18 -0400

    feature(src/routes/HomePage.tsx): ADD start button in component

--- pokeProject/src/routes/HomePage.tsx
+++ pokeProject/src/routes/HomePage.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const HomePage: React.FC = () => {
   return (
-    <section className="h-[500px] flex-col px-10">
-      <article className="mx-2 mb-4 flex flex-row justify-between">
-        <div>
+    <section className="h-[500px] flex justify-center px-10">
+      <article className="mx-4 mb-4 flex flex-row justify-center">
+        <div className="justify-center items-center flex-col h-full w-full">
           <h1> PokeWeb </h1>
+          <button onClick={() => console.log('CLICKED')}> START</button>
         </div>
       </article>
     </section>
