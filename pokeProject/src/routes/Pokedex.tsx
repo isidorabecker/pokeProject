e794a6b6658e4a40867cac6ab114e07ed43f8693
isidorabecker <isidora.becker@uc.cl>
2025-06-01 10:30:03 -0400

    feature(src/routes/Pokedex.tsx): ADD undefined type

--- pokeProject/src/routes/Pokedex.tsx
+++ pokeProject/src/routes/Pokedex.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { PokedexCard } from '../shared/PokedexCard'
-import { useGetPokemonInfo } from '../requests'
+import { useGetPokemonInfo, useGetPokemonDescription } from '../requests'
 
 export const Pokedex: React.FC = () => {
   const navigate = useNavigate()
   const { id: pokemonId } = useParams()
-  const { data: pokedexData, isLoading } = useGetPokemonInfo(pokemonId as string)
+  const { data: pokedexData, isLoading: loadingData } = useGetPokemonInfo(pokemonId as string)
+  const { data: pokemonDescription, isLoading: loadingDescription } = useGetPokemonDescription(pokemonId as string)
 
-  if (isLoading) {
+  if (loadingData || loadingDescription) {
     return <div className="flex justify-center items-center h-full">Loading...</div>
   }
   const { id, name, types, weight, height, sprites } = pokedexData
@@ -23,7 +24,7 @@ export const Pokedex: React.FC = () => {
       <div className="mt-3 flex justify-center font-semibold text-4xl mb-4">
         <span className="mb-8 text-center text-4xl font-extrabold text-red-600">Pokédex</span>
       </div>
-      <div className="flex justify-center mb-4">
+      <div className="flex gap-3 justify-center mb-4">
         <PokedexCard
           id={id}
           name={name}
@@ -31,11 +32,12 @@ export const Pokedex: React.FC = () => {
           weight={weight}
           height={height}
           imageUrl={sprites.front_default}
+          description={pokemonDescription}
         />
       </div>
       <div className="flex justify-center mb-3">
         <button
-          className="bg-red-600 border-radious items-center rounded-lg w-1/2 h-10 text-white font-semibold"
+          className="bg-red-600 border-radious items-center rounded-lg w-[500px] h-10 text-white font-semibold"
           onClick={() => handleBack()}
         >
           Atrás
