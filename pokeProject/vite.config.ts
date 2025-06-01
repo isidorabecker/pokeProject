74d322ae7c191293e435b5d69072f099456aff54
isidorabecker <isidora.becker@uc.cl>
2025-06-01 16:40:57 -0400

    feature(src/setupTests.ts): ADD setup test file

--- pokeProject/vite.config.ts
+++ pokeProject/vite.config.ts
@@ -4,4 +4,10 @@ import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [react(), tailwindcss('../tailwind.config.js') as any],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    css: true
+  }
 })
