868c49bc705554a3fea2db16b440a25a3671add4
isidorabecker <isidora.becker@uc.cl>
2025-06-01 10:28:50 -0400

    feature(src/requests.tsx): ADD useGetPokemonDescription request

--- pokeProject/src/requests.tsx
+++ pokeProject/src/requests.tsx
@@ -1,6 +1,6 @@
 import { useQuery, type  UseQueryResult } from '@tanstack/react-query';
 
-const pokeApiUrl = 'https://pokeapi.co/api/v2/pokemon/';
+const pokeApiUrl = 'https://pokeapi.co/api/v2/';
 
 export interface Pokemon {
   id: number
@@ -14,7 +14,7 @@ interface PokemonResponse {
 
 export const useGetPokemonDetails = (): UseQueryResult<Pokemon[], Error> => {
   async function getPokemonDetails(): Promise<Pokemon[]> {
-    const response = await fetch(`${pokeApiUrl}?limit=30`)
+    const response = await fetch(`${pokeApiUrl}pokemon/?limit=30`)
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
@@ -44,7 +44,7 @@ export const useGetPokemonDetails = (): UseQueryResult<Pokemon[], Error> => {
 
 export const useGetPokemonInfo = (id: string): UseQueryResult<any, Error> => {
   async function getPokemonInfo(): Promise<Pokemon> {
-    const response = await fetch(`${pokeApiUrl}/${id}`)
+    const response = await fetch(`${pokeApiUrl}pokemon/${id}`)
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`)
     }
@@ -55,5 +55,23 @@ export const useGetPokemonInfo = (id: string): UseQueryResult<any, Error> => {
   return useQuery({
     queryKey: ['pokemonInfo', id],
     queryFn: async () => await getPokemonInfo()
+  })
+}
+
+export const useGetPokemonDescription = (id: string): UseQueryResult<string, Error> => {
+  async function getPokemonDescription(): Promise<string> {
+    const response = await fetch(`${pokeApiUrl}pokemon-species/${id}`)
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`)
+    }
+    
+    const data = await response.json()
+    const description = data.flavor_text_entries.find((entry: any) => entry.language.name === 'es')
+    return description ? description.flavor_text : 'No tiene una descripción disponible en español.'
+  }
+
+  return useQuery({
+    queryKey: ['pokemonDescription', id],
+    queryFn: async () => getPokemonDescription()
   })
 }
