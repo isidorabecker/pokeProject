c11a3084e98a80066a7fcdf33013838c227d9150
isidorabecker <isidora.becker@uc.cl>
2025-06-01 10:28:16 -0400

    feature(src/shared/PokedexCard.tsx): ADD description in component

--- pokeProject/src/shared/PokedexCard.tsx
+++ pokeProject/src/shared/PokedexCard.tsx
@@ -1,6 +1,5 @@
 
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export interface PokedexCardProps {
   id: number
@@ -9,6 +8,7 @@ export interface PokedexCardProps {
   height: number
   weight: number
   imageUrl: string
+  description: string
 }
 
 export const PokedexCard: React.FC<PokedexCardProps> = ({
@@ -17,46 +17,51 @@ export const PokedexCard: React.FC<PokedexCardProps> = ({
   types,
   height,
   weight,
-  imageUrl
+  imageUrl,
+  description
 }: PokedexCardProps) => {
   return (
-    <Link to={`/pokemons/${id}`}>
-      <div className="flex flex-col w-[600px] h-96 p-4 border-2 rounded-lg shadow-md bg-green-200 hover:shadow-lg transition-shadow">
-        <h3 className="flex justify-center mt-2 text-xl text-black font-semibold capitalize">
-          #{id} {name}
-        </h3>
-        <div className="mt-3 flex w-full">
-          <div className="w-1/2 flex justify-center items-center">
-            <div className="w-40 p-4 border-2 rounded-lg shadow-md bg-white hover:shadow-lg transition-shadow">
-              <img
-                src={imageUrl}
-                alt={name}
-                className="w-32 h-32 object-contain"
-              />
-            </div>
+    <div className="flex flex-col w-[500px] h-96 p-4 border-2 rounded-lg shadow-md bg-green-200">
+      <h3 className="flex justify-center mt-2 text-xl text-black font-semibold capitalize">
+        #{id} {name}
+      </h3>
+      <div className="mt-3 flex w-full">
+        <div className="w-1/2 flex justify-center items-center">
+          <div className="w-40 p-4 border-2 rounded-lg shadow-md bg-white">
+            <img
+              src={imageUrl}
+              alt={name}
+              className="w-32 h-32 object-contain"
+            />
           </div>
-          <div className="w-1/2 flex flex-col justify-center items-start pl-4">
-            <div className="flex flex-wrap gap-2">
-              {types.map((type) => (
-                <span
-                  key={type}
-                  className="px-3 py-1 text-lg font-semibold text-white bg-red-600 rounded"
-                >
-                  {type.toUpperCase()}
-                </span>
-              ))}
-            </div>
-            <div className="mt-2">
-              <span className="text-black text-lg font-semibold">Height: </span>
-              <span className="text-black text-lg">{height}</span>
-            </div>
-            <div className="mt-2">
-              <span className="text-black text-lg font-semibold">Weight: </span>
-              <span className="text-black text-lg">{weight}</span>
-            </div>
+        </div>
+        <div className="w-1/2 flex flex-col justify-center items-start pl-4">
+          <div className="flex flex-wrap gap-2">
+            {types.map((type) => (
+              <span
+                key={type}
+                className="px-3 py-1 text-lg font-semibold text-white bg-red-600 rounded"
+              >
+                {type.toUpperCase()}
+              </span>
+            ))}
+          </div>
+          <div className="mt-2">
+            <span className="text-black text-lg font-semibold">Altura: </span>
+            <span className="text-black text-lg">{height}</span>
+          </div>
+          <div className="mt-2">
+            <span className="text-black text-lg font-semibold">Peso: </span>
+            <span className="text-black text-lg">{weight}</span>
           </div>
         </div>
       </div>
-    </Link>
+      <div 
+        className =
+          "mt-5 w-full p-4 border-2 rounded-lg shadow-md bg-white text-black font-medium text-center"
+      >
+        {description}
+      </div>
+    </div>
   )
 }
